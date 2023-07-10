46d771932c3e382e983ffda13d2b7fdb77ef6f54
vitor <vitor.53@gmail.com>
2023-07-10 17:24:52 -0300

    teste

--- cap02/js/ex2_1.js
+++ cap02/js/ex2_1.js
@@ -1,6 +1,7 @@
-function mostrarOla() {
-    var nome = document.getElementById("nome").value;
-    document.getElementById("resposta").textContent = "Olá " + nome;
-}
-var mostrar = document.getElementById("mostrar");
-mostrar.addEventListener("click", mostrarOla);
+//criar refêrencia ao form e ao elemento h3 ( onde será exibido a resposta)
+    const form = document.querySelector("form")
+    const resp = document.querySelector("h3")
+//criar um "ouvinte" de evento, acionado quando o botão submit for clicado
+frameElement.addEventListener("submit", (e) => {
+const nome = frm.inNome.value   //obtém o nome digitando no form
+resp.innerText= 'Olá ${nome}'   //exibe a resposta do programa
