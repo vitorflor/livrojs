764a72df7741e5e17436b22c2f02ff7e6a34699d
vitor <vitor.53@gmail.com>
2023-07-10 17:33:28 -0300

    teste

--- cap02/js/ex2_1.js
+++ cap02/js/ex2_1.js
@@ -2,6 +2,6 @@
     const form = document.querySelector("form")
     const resp = document.querySelector("h3")
 //criar um "ouvinte" de evento, acionado quando o botão submit for clicado
-frameElement.addEventListener("submit", (e) => {
+frm.addEventListener("submit", (e) => {
 const nome = frm.inNome.value   //obtém o nome digitando no form
 resp.innerText= 'Olá ${nome}'   //exibe a resposta do programa
