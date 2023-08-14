9506c7cca90ce0c94f301a99cce483e1d94b3766
vitor <vitor.flor53@gmail.com>
2023-08-13 23:42:09 -0300

    capitulo 4

---
+++ cap04/js/exe4_1.js
@@ -0,0 +1,25 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const nome = frm.inNome.value
+    const nota1 = Number(frm.inNota1.value)
+    const nota2 = Number(frm.inNota2.value)
+    //calculo
+    const media = (nota1 + nota2) / 2
+    //resposta da media
+    resp1.innerText = `media da(o) ${nome} é: ${media.toFixed(2)}`
+    //cria as condições
+    if (media >= 7) {
+    resp2.innerText = `parabens voce foi aprovado(a)`
+    resp2.style.color = "green"
+    } else if (media<=4) {
+        resp2.innerText = `Você está em exame`
+        resp2.style.color = "yellow"
+    } else{
+        resp2.innerText = `Voce foi reprovado(a)`
+        resp2.style.color = "red"
+    }
+})
