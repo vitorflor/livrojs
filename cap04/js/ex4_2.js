1a733d9fc1e0364e6718a59874a10071e49698b6
vitor <vitor.flor53@gmail.com>
2023-08-14 21:57:50 -0300

    rsg

---
+++ cap04/js/ex4_2.js
@@ -0,0 +1,19 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const nome = frm.inNome.value
+    const masculino = frm.inMasculino.checked
+    const altura = Number(frm.inAltura.value)
+
+    let peso
+    if (masculino){
+        peso = 22 * Match.pow(altura, 2)
+    } else {
+        peso = 21 * Match.pow(altura, 2)
+    }
+
+resp.innerText = `${nome}: Seu peso ideal é ${peso.toFixed(3)} kg`
+})
