b74d9670a84870520f39b655eaf2d5afa93f2516
vitor <vitor.flor53@gmail.com>
2023-08-16 20:25:30 -0300

    sdf

--- cap01/teste.js
+++
@@ -1,9 +0,0 @@
-let fruta
-    let preco
-    let levar 
-    let novoValor 
-
-    console.log(typeof fruta)     // string
-    console.log(typeof preco)     // number
-    console.log(typeof levar)     // boolean
-    console.log(typeof novoValor) // undefined
