a9412158a6f8b0324dd089a515cabc865ddc6dc4
vitor <vitor.flor53@gmail.com>
2023-08-16 20:38:05 -0300

    ex4.4

--- cap04/js/ex4_4.js
+++ cap04/js/ex4_4.js
@@ -1,10 +1,14 @@
-const form = document.querySelector("form")
+const frm = document.querySelector("form")
 const resposta = document.querySelector("h3")
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    const horaBrazil = frm.inHora.value
-
-    const Fuso = Number(horaBrazil + 5)
+    const horaBrazil = Number(frm.inHora.value)
+//calculo
+    let Fuso = horaBrazil + 5
+    if (Fuso > 24){
+    Fuso = Fuso - 24
+    }
+    resposta.innerText = `Hora na Franç: ${Fuso.toFixed(2)}`
 })
