1808b13208ecd58ddb14409a204972120db41749
vitor <vitor.flor53@gmail.com>
2023-08-17 22:40:53 -0300

    ex4_7.js

---
+++ cap04/nodejs/ex4_7.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync")()
+const pessoas = Number(prompt("Número de pessoas:"))
+const peixes = Number(prompt("Numero de peixes:"))
+let pagar
+if(peixe <= pessoas) {
+pagar = pessoas * 20
+} else {
+const contadepessoas = pessoas * 20
+const contadopeixe = (pessoas - peixes) *12 
+pagar = contadepessoas + contadopeixe
+}
+console.log(`Pagar: ${apagar.toFixed(2)}`)
