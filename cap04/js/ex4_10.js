697455d8e446bb6e18fb585e32e969adcb39f6e2
vitor <vitor.flor53@gmail.com>
2023-08-21 19:43:42 -0300

    arrumando o ex_10

--- cap04/js/ex4_10.js
+++ cap04/js/ex4_10.js
@@ -1,13 +1,14 @@
-const frm = document.querySelctor("form")
+const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
+
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
-    const numero = Number(frm.inNumero.value)
-//calculo
-if(numero / 2 == 0) {
-    resp.innerText=`${numero} é par`
-} else {
-    resp.innerText=`${numero} é impár`
-}
-})
+    
+    const num = Number(frm.inNumero.value)
+    if (num % 2 === 0) {
+        resp.innerText= `Seu número e par`
+    } else {
+        resp.innerText= `Seu número e ímpar`
+    }
+    })
