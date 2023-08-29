de2cbc4a4ff308c96a6080062d604aa21d0b98cb
vitor flor <vitor.flor53@gmail.com>
2023-08-29 15:32:26 -0300

    correção

--- cap04/js/ex4_12.js
+++ cap04/js/ex4_12.js
@@ -1,19 +1,26 @@
 const frm = document.querySelector("form")
 const resp1 = document.querySelector("#outresp1")
-const resp2 =document.querySelector("#outresp2")
+const resp2 = document.querySelector("#outresp2")
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault() 
     const valor = Number(frm.inValor.value)
+    if (valor > 1) {
+        alert("Valor Invalido")
+        frm.inValor.focus()
+        return
+    }
     const troco1 = valor - 1 
     const troco2 = valor - 1.75
     const troco3 = valor - 3
-    if(valor < 1){
-        resp1.innertext= `Valor Insuficiente`
-    } else if(valor <= 1) {
+    if(valor <= 1) {
         resp1.innertext= `tempo: 30min`
         resp2.innertext= `troco: ${troco1.toFixed(2)}`
-    } else if(valor <= 1.75)
+    } else if(valor <= 1.75) {
     resp1.innertext= `tempo: 30min`
     resp2.innertext= `troco: ${troco2.toFixed(2)}`
+    } else if(valor <= 3){
+    resp1.innertext= `tempo: 30min`
+    resp2.innertext= `troco: ${troco3.toFixed(2)}`
+    }
 })
