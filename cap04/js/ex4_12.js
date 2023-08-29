2c86ac8cbd0b209def14a26681049e11a379dd1c
vitor flor <vitor.flor53@gmail.com>
2023-08-29 15:21:09 -0300

    ex12_4

---
+++ cap04/js/ex4_12.js
@@ -0,0 +1,19 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outresp1")
+const resp2 =document.querySelector("#outresp2")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault() 
+    const valor = Number(frm.inValor.value)
+    const troco1 = valor - 1 
+    const troco2 = valor - 1.75
+    const troco3 = valor - 3
+    if(valor < 1){
+        resp1.innertext= `Valor Insuficiente`
+    } else if(valor <= 1) {
+        resp1.innertext= `tempo: 30min`
+        resp2.innertext= `troco: ${troco1.toFixed(2)}`
+    } else if(valor <= 1.75)
+    resp1.innertext= `tempo: 30min`
+    resp2.innertext= `troco: ${troco2.toFixed(2)}`
+})
